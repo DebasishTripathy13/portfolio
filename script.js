c5f2a496f04a0b3589053be663781b066bb5459f
debas <23212@apsrkpuram.edu.in>
2024-02-09 22:59:09 +0530

    commit by debasish

---
+++ script.js
@@ -0,0 +1,5 @@
+// For demonstration only - you would handle cart, product data, etc.
+const cartButton = document.querySelector('nav li:last-child'); 
+cartButton.addEventListener('click', () => {
+    alert('Cart functionality would be implemented here!');
+});
